fac7266c4483408c06098a73256a92000ffb3185
incertia <incertia@incertia.net>
2025-11-10 22:19:20 -0500

    add quickfix and tabs to fzf

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -2,8 +2,9 @@ local km = vim.keymap.set
 
 -- fzf
 km("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
--- km("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
+km("n", "<leader><C-k>", [[<Cmd>lua require"fzf-lua".quickfix()<CR>]], {})
 km("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
+km("n", "<leader><C-t>", [[<Cmd>lua require"fzf-lua".tabs()<CR>]], {})
 km("n", "<C-m>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
 km("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
 -- km("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
